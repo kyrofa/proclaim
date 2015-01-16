cf80a3f8f15f0f4595f7dd1196b3d8e5776f620a
Kyle Fazzari <proclaim@status.e4ward.com>
2015-01-16 09:33:37 -0500

    Fixed missing "config" in initializer template.

--- lib/generators/proclaim/templates/initialize_proclaim.rb
+++ lib/generators/proclaim/templates/initialize_proclaim.rb
@@ -3,23 +3,23 @@ Proclaim.setup do |config|
 	# `current_author_method` and `authentication_method`. For example, setting
 	# `author_class = "Admin"` changes the default `current_author_method` to be
 	# `:current_admin`, etc.
-	#author_class = "User"
+	#config.author_class = "User"
 
 	# Method to obtain the name of the author. This should be a method on the
 	# author class.
-	#author_name_method = :name
+	#config.author_name_method = :name
 
 	# Method to obtain the currently-authenticated user. Should return nil if
 	# no user is currently authenticated.
-	#current_author_method = :current_user
+	#config.current_author_method = :current_user
 
 	# Method to verify that a user is authenticated, and if not, will redirect
 	# to some sort of authentication page.
-	#authentication_method = :authenticate_user!
+	#config.authentication_method = :authenticate_user!
 
 	# Maximum length for the excerpts shown on the posts index.
-	#excerpt_length = 500
+	#config.excerpt_length = 500
 
 	# Buttons to display on post editor toolbar
-	#editor_toolbar_buttons = ['bold', 'italic', 'underline', 'anchor', 'header1', 'header2', 'quote']
+	#config.editor_toolbar_buttons = ['bold', 'italic', 'underline', 'anchor', 'header1', 'header2', 'quote']
 end
